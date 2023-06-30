7c9978d373ea25745abbaa0d0b733025b0684dee
Braien Machado de Paula <braienmp@outlook.com>
2023-06-30 01:57:31 -0300

    feat: create initial endpoints for presentations - POST presentation - GET presentations

---
+++ src/app/api/presentations/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Key = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G';
+
+type Song = {
+  id?: string
+  name: string
+  author: string
+  videoUrl?: string
+  key?: Key
+}
+
+export async function GET (_request: Request) {
+  const result = await prisma.presentation.findMany({
+    include: { songs: { include: { song: true } } }
+  });
+  return NextResponse.json(result);
+}
+
+export async function POST (request: Request) {
+  try {
+    const { date, songs } = await request.json();
+    const presentation = await prisma.$transaction(async (tx) => {
+      const createdSongs = await Promise.all(songs.map(async ({ name, author, key, videoUrl }: Song) => {
+        const createdSong = await tx.song.create({ data: { name, author, videoUrl } });
+        return { key, songId: createdSong.id };
+      }));
+  
+      return tx.presentation.create({
+        data: {
+          date: new Date(date),
+          songs: {
+            createMany: {
+              data: createdSongs
+            }
+          }
+        }
+      })
+    });
+  
+    return new Response(JSON.stringify(presentation), { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+}
