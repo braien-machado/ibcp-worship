92bce97d4cd95f5626576b6c1012cebf73fbfe3f
Braien Machado de Paula <braienmp@outlook.com>
2023-07-20 22:46:11 -0300

    chore: change home text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "IBCP Worship App"
+  title: "IBCP - Página Inicial"
 }
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>IBCP Worship App</h1>
-      <p>Coming soon...</p>
+      <h1>IBCP</h1>
+      <h3>Igreja Batista em Conselheiro Paulino</h3>
+      <p>Em breve...</p>
     </main>
   )
 }
